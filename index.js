01878f632a8d0face3490fd85a925e748743e832
N. J <datavispresent@gmail.com>
2023-06-20 23:40:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ app.get('/download', async (req, res) => {
     
     const projectId = '47023085'; /// replace with your project ID
     const directory = 'settrade_eod_data'; // directory inside the GitLab project
-    const fileName = `${symbol}.json`; // replace `symbol` with the symbol of the data you're uploading
+    const fileName = `${res["symbol"]}.json`; // replace `symbol` with the symbol of the data you're uploading
     const content = jsonData; // replace `jsonData` with the actual JSON data you want to upload
     const commitMessage = `Add ${symbol} data`;
     const gitlabToken = 'glpat-QifxpbGDzh1F8--AHgN6'; // replace with your actual GitLab token
